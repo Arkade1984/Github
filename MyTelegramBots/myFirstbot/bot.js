e982dc1fc0aec5b880539926ddd28c514daf6bfb
Valentino Figueroa <clinteastwoocl@yahoo.com>
2021-07-30 21:52:14 -0700

    features

--- MyTelegramBots/myFirstbot/bot.js
+++ MyTelegramBots/myFirstbot/bot.js
@@ -1,24 +1,34 @@
-const Telegraf =  require('telegraf');
+// const Telegraf =  require('telegraf');
 
-const bot =  new Telegraf('1888442275:AAGj_aKQEKvBXRj5f6FKQp-NGV23BLjqSlo');
+// const bot =  new Telegraf('1888442275:AAGj_aKQEKvBXRj5f6FKQp-NGV23BLjqSlo');
 
-//          /start
-bot.start((ctx) => {
-ctx.reply(ctx.from.first_name + 'You have entered the start command' + ctx.updateSubTypes[0]);
+// //          /start
+// bot.start((ctx) => {
+// ctx.reply(ctx.from.first_name + 'You have entered the start command' + ctx.updateSubTypes[0]);
 
-})
+// })
 
-bot.help((ctx) => {
-    ctx.reply('You have entered the help command');
+// bot.help((ctx) => {
+//     ctx.reply('You have entered the help command');
 
-})
+// })
 
-bot.settings((ctx) => {
-    ctx.reply('You have entered the settings commands')
-})
+// bot.settings((ctx) => {
+//     ctx.reply('You have entered the settings commands')
+// })
 
-bot.command(["test", "Test"], (ctx) => {
-    cty.reply("Hello World");
-})
+// bot.command(["test", "Test"], (ctx) => {
+//     cty.reply("Hello World");
+// })
+
+// bot.launch();
+
+bot.hears("cat", (ctx) => (
+    ctx.reply("Meow");
+))
+
+bot.on("stickers", (ctx) => (
+    ctx.reply("This is a sticker message");
+))
 
 bot.launch();
